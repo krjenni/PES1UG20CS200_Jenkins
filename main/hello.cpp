58b4553283aaeabb8e24b22f21e3a58275fa1054
Karthikeya R Jenni <79406866+krjenni@users.noreply.github.com>
2023-02-17 09:45:04 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,7 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout<<"PES1UG20CS200";
-  cout<<"changes made";
+  cout<<"PES1UG20CS200\n";
+  cout<<"changes made\n";
   return 0;
 }
